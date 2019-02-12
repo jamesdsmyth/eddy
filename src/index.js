65ee61bb9120b8c4d9fa12088ab9e327a411cabe
James <smythie_89@hotmail.com>
2019-02-12 08:35:00 +0400

    Set up continued

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import "./scss/main.scss";
+// import "./scss/main.scss";
+
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-
-class App extends React.Component {
-  render() {
+class App extends Component {
+render() {
     return (
       <div>
-        'Hello world!'
+        inside
       </div>
     )
   }
 }
-
 render(<App />, document.getElementById('app'));
